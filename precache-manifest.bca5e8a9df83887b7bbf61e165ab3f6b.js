a6e436c5e3577a6dcf3b679c012d42c7063146de
Volodsher <volodsher@gmail.com>
2019-09-25 09:40:26 +0300

    Updates

---
+++ precache-manifest.bca5e8a9df83887b7bbf61e165ab3f6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf177e69e91a77606d18ade1dcfef3c7",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "d08a19edef13837699a7",
+    "url": "/react_dynamic-list-of-todos/static/css/main.95d56949.chunk.css"
+  },
+  {
+    "revision": "d33b7e54301026dba848",
+    "url": "/react_dynamic-list-of-todos/static/js/2.d3072c5d.chunk.js"
+  },
+  {
+    "revision": "d08a19edef13837699a7",
+    "url": "/react_dynamic-list-of-todos/static/js/main.41dc87a9.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
